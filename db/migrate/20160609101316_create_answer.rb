26d4b70b6d769c2be6d3000e69c70fa8bff1b370
Noah Naamad <email": null>
2016-06-09 10:15:57 -0700

    create answer migration

---
+++ db/migrate/20160609101316_create_answer.rb
@@ -0,0 +1,11 @@
+class CreateAnswer < ActiveRecord::Migration
+   def change
+    create_table :answers do |t|
+    t.integer :question_id, null: false
+    t.integer :user_id, null: false
+    t.string :content, null: false, length: {maximum: 5000}
+
+    t.timestamps(null: false)
+    end
+  end
+end
