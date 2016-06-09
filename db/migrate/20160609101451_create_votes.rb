3d92687fb36bd56425c985b61827de1809c931c5
Michael Dumalag & Mandy McDorman <pair+michaeldumalag.whatthemandy@devbootcamp.com>
2016-06-09 10:51:14 -0700

    fix spacing

--- db/migrate/20160609101451_create_votes.rb
+++ db/migrate/20160609101451_create_votes.rb
@@ -1,11 +1,12 @@
 class CreateVotes < ActiveRecord::Migration
   def change
   	create_table :votes do |t|
-  		t.integer	:voteable_id, null: false 
-  		t.string	:votable_type, null: false 
-  		t.integer	:user_id, null: false 
-  		t.integer	:upvote, default: 0
-  		t.timestamps(null: false)
+  	  t.integer	:voteable_id, null: false 
+			t.string	:votable_type, null: false 
+			t.integer	:user_id, null: false 
+			t.integer	:upvote, default: 0
+			
+			t.timestamps(null: false)
   	end
   end
 end
