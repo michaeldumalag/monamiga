f7da75862b6147326445583d5164b5a790cefcc3
Michael Dumalag & Mandy McDorman <pair+michaeldumalag.whatthemandy@devbootcamp.com>
2016-06-09 14:52:01 -0700

    Add polymorphic associations for responses, questions, answers - created uniquenesfor users

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,6 +2,6 @@ class Vote < ActiveRecord::Base
 
   belongs_to	:voteable, polymorphic: true
 
-  validates_uniqueness_of :voteable, scope: :user 
+  validates :voteable_id, uniqueness: {scope: [:user_id, :voteable_type]} 
 
 end
