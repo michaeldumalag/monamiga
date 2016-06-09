d213b913435d91ef55461611bab0f5b88f4a3f96
Michael Dumalag & Mandy McDorman <pair+michaeldumalag.whatthemandy@devbootcamp.com>
2016-06-09 11:07:54 -0700

    Set up models for polymorphism, votes and responses

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,9 @@
 class Answer < ActiveRecord::Base
-  # Remember to create a migration!
-  validates :question_id, :user_id, presence:true
-
+  
   belongs_to :user
   belongs_to :question
+  has_many	 :responses, as: :respondable		
+
+  validates  :question_id, :user_id, presence:true
+
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,9 @@
 class Question < ActiveRecord::Base
 
   belongs_to :user
+  has_many	 :responses, as: :respondable
 
-  validates :title, presence: true
-  validates :content, presence: true
+  validates  :title, presence: true
+  validates  :content, presence: true
 
 end
